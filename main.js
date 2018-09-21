9a48133dedab26b28ab00fc685bcf7a16f921836
Jaeho Lee <jaeho7150@gmail.com>
2018-09-21 20:32:31 +0900

    Challenge 3 Jaeho
    I struggled with this challenge. It was very hard and I didn't complete all of tasks but I enjoyed it which is good.

---
+++ main.js
@@ -0,0 +1,18 @@
+
+console.log('app is alive')
+console.log('Tuning into channel' + channelName)
+function switchChannel(channelName)
+
+function selectTab(){
+
+}
+
+function showemojis(){
+$('#emojis').show();
+
+togglebutton();
+}
+
+function togglebutton(){
+    $('#chat-bar button').toggle();
+}
